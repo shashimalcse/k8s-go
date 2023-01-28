906cb87208e5f92af1d30ffe5463cdf2c5ac6464
Thilina Shashimal Senarath <43197743+shashimalcse@users.noreply.github.com>
2023-01-28 08:39:56 +0000

    add hostname

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		hostname, err := os.Hostname()
+
+		if err != nil {
+			fmt.Println("Error getting hostname")
+		}
+
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusCreated)
-		json.NewEncoder(w).Encode("k8s-go-server")
+		json.NewEncoder(w).Encode("k8s-go hostname : " + hostname)
 	})
 
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
